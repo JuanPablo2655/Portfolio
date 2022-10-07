9514c8a7642c5d4d6973c239179af811d266e76c
JuanPablo2655 <personal@isidrocasiano.com>
2022-10-07 19:27:12 -0400

    fix: flash of dark mode

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const button = document.querySelector('#mode');
 
 const dark = localStorage.getItem('dark') === 'true';
-setup(dark);
-console.log(dark);
 
 button?.addEventListener('click', () => {
 	if (dark) {
@@ -10,26 +8,12 @@ button?.addEventListener('click', () => {
 		document.querySelector(':root')?.style.setProperty('--primary-bg-color', '#ECEFF4');
 		document.querySelector(':root')?.style.setProperty('--secondary-color', '#88C0D0');
 		localStorage.setItem('dark', 'false');
-		console.log(localStorage.getItem('dark'));
 		location.reload();
 	} else {
 		document.querySelector(':root')?.style.setProperty('--primary-color', '#E5E9F0');
 		document.querySelector(':root')?.style.setProperty('--primary-bg-color', '#2E3440');
 		document.querySelector(':root')?.style.setProperty('--secondary-color', '#88C0D0');
 		localStorage.setItem('dark', 'true');
-		console.log(localStorage.getItem('dark'));
 		location.reload();
 	}
 });
-
-function setup(dark) {
-	if (dark) {
-		document.querySelector(':root')?.style.setProperty('--primary-color', '#3B4252');
-		document.querySelector(':root')?.style.setProperty('--primary-bg-color', '#ECEFF4');
-		document.querySelector(':root')?.style.setProperty('--secondary-color', '#88C0D0');
-	} else {
-		document.querySelector(':root')?.style.setProperty('--primary-color', '#E5E9F0');
-		document.querySelector(':root')?.style.setProperty('--primary-bg-color', '#2E3440');
-		document.querySelector(':root')?.style.setProperty('--secondary-color', '#88C0D0');
-	}
-}
