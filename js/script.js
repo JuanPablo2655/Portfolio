1e901ee03af3514d241ab6641a69ea87b726a1a3
Isidro Casiano <personal@isidrocasiano.com>
2022-10-07 19:32:58 -0400

    feat: refresh
    * feat: init
    
    * feat: working theme switcher
    
    * fix: flash of dark mode
    
    * chore: organize files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('#mode');
+
+const dark = localStorage.getItem('dark') === 'true';
+
+button?.addEventListener('click', () => {
+	if (dark) {
+		document.querySelector(':root')?.style.setProperty('--primary-color', '#3B4252');
+		document.querySelector(':root')?.style.setProperty('--primary-bg-color', '#ECEFF4');
+		document.querySelector(':root')?.style.setProperty('--secondary-color', '#88C0D0');
+		localStorage.setItem('dark', 'false');
+		location.reload();
+	} else {
+		document.querySelector(':root')?.style.setProperty('--primary-color', '#E5E9F0');
+		document.querySelector(':root')?.style.setProperty('--primary-bg-color', '#2E3440');
+		document.querySelector(':root')?.style.setProperty('--secondary-color', '#88C0D0');
+		localStorage.setItem('dark', 'true');
+		location.reload();
+	}
+});
