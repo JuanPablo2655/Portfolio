cdaed052b36c70d50e7a6b8623f6fb570ea33d64
JuanPablo2655 <personal@isidrocasiano.com>
2022-10-10 17:02:41 -0400

    fix: handle missing variable in local storage

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,4 @@
-const isDark = localStorage.getItem('dark') === 'true';
+const isDark = localStorage.getItem('dark') === 'true' ?? true;
 if (isDark) {
 	document.querySelector(':root')?.style.setProperty('--primary-color', '#E5E9F0');
 	document.querySelector(':root')?.style.setProperty('--primary-bg-color', '#2E3440');
