11e7de0d2bc01dac8e1bf67b07bd19189dad7299
JuanPablo2655 <personal@isidrocasiano.com>
2023-01-04 19:31:43 -0500

    fix: age

--- js/script.js
+++ js/script.js
@@ -13,5 +13,23 @@ button?.addEventListener('click', () => {
 });
 
 const p = document.querySelector('p');
-const age = new Date().getFullYear() - 2002;
+const rawAge = new Date().now() - new Date('2002-05-10').getTime();
+const age = parseMilliseconds(rawAge).years;
 p.innerText = `I'm just a ${age} year old programmer`;
+
+function parseMilliseconds(milliseconds) {
+	if (typeof milliseconds !== 'number') {
+		throw new TypeError('Expected a number');
+	}
+
+	return {
+		years: Math.trunc(milliseconds / 31557600000),
+		days: Math.trunc(milliseconds / 86400000),
+		hours: Math.trunc(milliseconds / 3600000) % 24,
+		minutes: Math.trunc(milliseconds / 60000) % 60,
+		seconds: Math.trunc(milliseconds / 1000) % 60,
+		milliseconds: Math.trunc(milliseconds) % 1000,
+		microseconds: Math.trunc(milliseconds * 1000) % 1000,
+		nanoseconds: Math.trunc(milliseconds * 1e6) % 1000,
+	};
+}
